ccf28c6fad1da12938d6ba72a3853d1cecb78317
Ubuntu <ubuntu@srv474510.hstgr.cloud>
2024-04-13 13:50:38 +0000

    first initial commit after creating some doctypes

---
+++ karobar/karobar/doctype/auppliers/auppliers.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Power Soft and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class Auppliers(Document):
+	pass
---
+++ karobar/karobar/doctype/sales_invoice/sales_invoice.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Power Soft and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Sales Invoice", {
+// 	refresh(frm) {
+
+// 	},
+// });
