86c6544169d709594319ddf705874456a32daf7c
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-04-16 16:31:26 -0400

    Invoice items calcuation is done.

--- karobar/karobar/doctype/sales_invoice/sales_invoice.js
+++ karobar/karobar/doctype/sales_invoice/sales_invoice.js
@@ -1,8 +1,55 @@
 // Copyright (c) 2024, Power Soft and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Sales Invoice", {
-// 	refresh(frm) {
+frappe.ui.form.on("Sales Invoice", {
+	// refresh(frm) {
 
-// 	},
-// });
+	// },
+
+
+});
+
+frappe.ui.form.on("Invoice Items", {
+	// refresh(frm) {
+	// },
+
+    item_name(frm, cdt, cdn)
+	{
+		// frappe.msgprint('Item name has been selected.');
+		let row = frappe.get_doc(cdt, cdn);
+		//console.log(row.item_name);
+		frappe.call({
+			method: "frappe.client.get",
+			args: {
+				doctype: "Item",
+				name: row.item_name,
+			},
+			callback(i) {
+				//console.log(i);
+				
+			}
+		});
+	},
+
+	// calculate value on change of qty
+	box_quantity(frm, cdt, cdn)
+	{		
+		calculate(frm, cdt, cdn);
+	},
+
+	// calculate value on change of rate
+	rate(frm, cdt, cdn)
+	{		
+		calculate(frm, cdt, cdn);
+	}
+});
+
+//calculation for total amount
+function calculate(frm, cdt, cdn)
+{
+	var qty = frappe.model.get_value(cdt, cdn, 'box_quantity');
+	var rate = frappe.model.get_value(cdt, cdn, 'rate');
+	
+	frappe.model.set_value(cdt, cdn, 'amount', parseFloat((qty*rate)));
+
+}
