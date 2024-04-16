a9e8d6d373aeaf97006740739b0c19039b5d1db4
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-04-16 10:38:58 -0400

    delete aupplier doc type

--- karobar/karobar/doctype/auppliers/test_auppliers.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, Power Soft and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestAuppliers(FrappeTestCase):
-	pass
