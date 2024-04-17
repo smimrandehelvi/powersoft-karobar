8adea94c3e8ed67dbde4f192acead5d46fba0dd2
Ubuntu <ubuntu@ubuntu-2204.linuxvmimages.local>
2024-04-17 17:36:30 -0400

    added ledger doctypes and coding for sending data to customers ledger

---
+++ karobar/karobar/doctype/general_ledger/test_general_ledger.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Power Soft and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGeneralLedger(FrappeTestCase):
+	pass
--- karobar/karobar/doctype/sales_invoice/sales_invoice.py
+++ karobar/karobar/doctype/sales_invoice/sales_invoice.py
@@ -1,9 +1,37 @@
 # Copyright (c) 2024, Power Soft and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class SalesInvoice(Document):
-	pass
+	# pass
+
+
+	def after_insert(self):
+
+		# create a new document in customer ledger
+		doc = frappe.get_doc({
+			'doctype': 'Customer Ledger',
+			'date': self.date,
+			'transaction_type': 'Invoice Receivable',
+			'reference': self.name,
+			'customer': self.customer_name,
+			'debit': self.net_amount,
+			'credit': 0,
+		})
+		doc.insert()
+	
+	def on_update(self):
+	
+		#get customer ledger entry related to this invoice
+		doc = frappe.get_last_doc('Customer Ledger', filters={
+    		'transaction_type' : 'Invoice Receivable',
+    		'reference' : self.name,
+		})
+		doc.date = self.date
+		doc.customer = self.customer_name
+		doc.debit = self.net_amount
+		doc.save()
+
